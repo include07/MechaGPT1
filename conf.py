3529f7a4f51028b29c72c2b50c32dfa40ecaba30
include07 <j.elfirqi@gmail.com>
2024-12-06 15:20:48 +0100

    modify conf.py

--- conf.py
+++ conf.py
@@ -6,8 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'MED-BOT'
-copyright = '2024, GIIA'
+project = 'MechaGPT'
 #author = 'GIIA'
 
 # -- General configuration ---------------------------------------------------
